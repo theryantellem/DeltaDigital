2806741de122216cbc7aeeb18f9179915741d4a8
DEv Noel <ndamjoh@gmail.com>
2023-07-20 06:17:16 +0100

    dn

--- database/migrations/2023_07_20_045931_add_address_to_wallets_table.php
+++ database/migrations/2023_07_20_045931_add_address_to_wallets_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('wallets', function (Blueprint $table) {
-            $table->string('address')->nullable();
+            $table->string('address')->nullable()->after('balance');
         });
     }
 
