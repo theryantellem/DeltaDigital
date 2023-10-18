379285ee00f600912a64b960cada5e3e2c7f05f7
DEv Noel <ndamjoh@gmail.com>
2023-10-18 21:18:04 +0100

    style added for test

--- resources/views/layouts/admin/partails/_head.blade.php
+++ resources/views/layouts/admin/partails/_head.blade.php
@@ -34,6 +34,12 @@
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
+    <style>
+        body{
+            background-color: #000;
+        }
+    </style>
+
 </head>
 
 
