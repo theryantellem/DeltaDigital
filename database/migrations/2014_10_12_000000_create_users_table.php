c4d115dd35e3a1ffa50910bf1737a742bd48922c
DEv Noel <ndamjoh@gmail.com>
2023-10-23 08:01:34 +0100

    primary key added to users model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->string('id')->primary()->unique();
+            $table->primary('id');
             $table->uuid('uuid');
             $table->string('username')->unique();
             $table->string('email')->unique();
