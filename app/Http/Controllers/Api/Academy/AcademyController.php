6873eea05bff3acd959eefb341c8e863751304d4
Angwa Moses <angwamoses@gmail.com>
2023-10-26 14:26:23 +0100

    Retrieve academy for only followed educators

--- app/Http/Controllers/Api/Academy/AcademyController.php
+++ app/Http/Controllers/Api/Academy/AcademyController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers\Api\Academy;
 use App\Http\Controllers\ApiController;
 use App\Http\Resources\Academy\AcademyResource;
 use App\Models\Academy;
+use App\Models\UserFollower;
 use Symfony\Component\HttpFoundation\Response;
 
 class AcademyController extends ApiController
 {
     public function index()
     {
-        $data = Academy::orderBy('id', 'desc')->get();
+        $user = auth()->user();
+        $adminIds = UserFollower::where('user_id', $user->id)->pluck('admin_id')->toArray();
+ 
+        $data = Academy::whereIn('admin_id', $adminIds)->orderBy('id', 'desc')->get();
         $resource = AcademyResource::collection($data);
 
         return $this->sendResponse($resource, "Academy list retrieved successfully.", Response::HTTP_OK);
