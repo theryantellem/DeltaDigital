3c2f5e20d30032519153a4c797f72f21c40b10e2
DEv Noel <ndamjoh@gmail.com>
2023-10-26 08:29:19 +0100

    updated

--- database/migrations/2023_10_25_234530_add_thumbnail_to_admins_table.php
+++ database/migrations/2023_10_25_234530_add_thumbnail_to_admins_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('admins', function (Blueprint $table) {
-            $table->string('thumbnail')->nullable()->after('photo');
+            $table->string('thumbnail')->nullable()->after('phone_number');
         });
     }
 
