eb4661a98017dd381bf5b8c7eaaecfef5b343aeb
DEv Noel <ndamjoh@gmail.com>
2023-10-31 14:21:49 +0100

    updated

--- app/Http/Controllers/Api/ScheduleController.php
+++ app/Http/Controllers/Api/ScheduleController.php
@@ -92,7 +92,7 @@ class ScheduleController extends ApiController
             }
 
             $schedule->update([
-                'viewers' =>  !$schedule->viewers - 1
+                'viewers' =>  $schedule->viewers > 0 ? $schedule->viewers - 1 : 0
             ]);
 
             LiveAttendants::where([
