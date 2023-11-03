2d462a180ea596c3b4faf631ee690491231a86cc
DEv Noel <ndamjoh@gmail.com>
2023-11-03 09:36:36 +0100

    port changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const events = require('./events/index')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
