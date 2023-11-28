8e0e76de365cb39f4d342a4ac8e89cdd698eb482
DEv Noel <ndamjoh@gmail.com>
2023-11-28 18:08:57 +0100

    updated

--- server.js
+++ server.js
@@ -21,39 +21,46 @@ io.on("connection", (socket) => {
     // Handle the chat message event
     socket.on(events.MESSAGE_SEND, async (data) => {
 
-        const { room, message, messages } = data
-        io.emit(`${events.MESSAGE_RECEIVED}:${room}`, { message: message, messages: messages })
+        const { room, message } = data
+        io.emit(`${events.MESSAGE_RECEIVED}:${room}`, message)
 
     });
 
     // start live
-    socket.on('start:live', async (educator) => {
-        console.log(educator)
+    socket.on('start:live', async (data) => {
+        const { educator } = data
 
         io.emit(`live:started`, educator)
 
     });
 
     // start live
-    socket.on('stop:live', async (educator) => {
+    socket.on('stop:live', async (data) => {
+
+        const { educator } = data
 
         io.emit(`live:stoped`, educator)
 
     });
 
     // Handle join stream
-    socket.on('join:stream', async (schedule) => {
+    socket.on('join:stream', async (data) => {
+        const { schedule } = data
+        
         io.emit(`joined:stream:${schedule}`, schedule)
 
     });
 
     // Leaving a steaming session
-    socket.on('leave:stream', async (schedule) => {
+    socket.on('leave:stream', async (data) => {
+        const { schedule } = data
+        // console.log(schedule)
         io.emit(`left:stream:${schedule}`, schedule)
     });
 
     // end streaming
-    socket.on('end:stream', async (schedule) => {
+    socket.on('end:stream', async (data) => {
+        const { schedule } = data
         io.emit(`stop:live:${schedule}`, schedule)
     });
 
