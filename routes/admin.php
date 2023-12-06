10cc979c7361b7c4d6379f296b9549104b18414a
DEv Noel <ndamjoh@gmail.com>
2023-12-06 06:42:49 +0100

    branch setup for just api resources

--- routes/admin.php
+++ routes/admin.php
@@ -36,189 +36,189 @@ use Kutia\Larafirebase\Facades\Larafirebase;
 |
 */
 
-Route::controller(AuthenticationController::class)->group(function () {
-    Route::get('/', 'login')->name('login');
-    Route::post('/', 'authenticate')->name('authenticate');
-    Route::get('/logout', 'logout')->name('logout')->middleware('auth:admin');
-});
+// Route::controller(AuthenticationController::class)->group(function () {
+//     Route::get('/', 'login')->name('login');
+//     Route::post('/', 'authenticate')->name('authenticate');
+//     Route::get('/logout', 'logout')->name('logout')->middleware('auth:admin');
+// });
 
-Route::middleware(['auth:admin'])->group(function () {
+// Route::middleware(['auth:admin'])->group(function () {
 
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+//     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::prefix('cyborg')->name('cyborg.')->group(function () {
-        Route::get('/get-exchangies', [ExchangeController::class, 'getExchanges'])->name('exchange.all');
+//     Route::prefix('cyborg')->name('cyborg.')->group(function () {
+//         Route::get('/get-exchangies', [ExchangeController::class, 'getExchanges'])->name('exchange.all');
 
-        Route::get('/get-strategies', [StrategyController::class, 'getStrategies'])->name('strategies.all');
-        Route::resource('strategy', StrategyController::class);
+//         Route::get('/get-strategies', [StrategyController::class, 'getStrategies'])->name('strategies.all');
+//         Route::resource('strategy', StrategyController::class);
 
-        Route::resource('trade-settings', TradeSettingsController::class);
+//         Route::resource('trade-settings', TradeSettingsController::class);
 
-        Route::resource('markets', MarketController::class);
-    });
+//         Route::resource('markets', MarketController::class);
+//     });
 
-    Route::get('get-signals', [SignalController::class, 'getSignals'])->name('signals.all');
-    Route::post('/update-market-status', [SignalController::class, 'updateMarketStatus'])->name('signals.market.status');
-    Route::post('/all-assets', [SignalController::class, 'getAssets'])->name('signals.assets');
-    Route::post('/update-status', [SignalController::class, 'updateStatus'])->name('signals.status');
-    Route::resource('signals', SignalController::class);
+//     Route::get('get-signals', [SignalController::class, 'getSignals'])->name('signals.all');
+//     Route::post('/update-market-status', [SignalController::class, 'updateMarketStatus'])->name('signals.market.status');
+//     Route::post('/all-assets', [SignalController::class, 'getAssets'])->name('signals.assets');
+//     Route::post('/update-status', [SignalController::class, 'updateStatus'])->name('signals.status');
+//     Route::resource('signals', SignalController::class);
 
-    Route::get('admins/all', [AdminManagementController::class, 'allAdmins'])->name('admins.all');
-    Route::resource('administrative', AdminManagementController::class);
+//     Route::get('admins/all', [AdminManagementController::class, 'allAdmins'])->name('admins.all');
+//     Route::resource('administrative', AdminManagementController::class);
 
-    Route::prefix('users')->name('users.')->controller(UserManagementController::class)->group(function () {
-        Route::get('/', 'index')->name('index');
-    });
+//     Route::prefix('users')->name('users.')->controller(UserManagementController::class)->group(function () {
+//         Route::get('/', 'index')->name('index');
+//     });
 
-    Route::resource('/news', NewsManagement::class);
+//     Route::resource('/news', NewsManagement::class);
 
-    Route::controller(SupportTicket::class)->prefix('tickets')->name('tickets.')->group(function () {
-        Route::get('tickets', 'index')->name('index');
-    });
+//     Route::controller(SupportTicket::class)->prefix('tickets')->name('tickets.')->group(function () {
+//         Route::get('tickets', 'index')->name('index');
+//     });
 
-    Route::get('educators/all', [EducatorController::class, 'allEducators'])->name('educators.all');
-    Route::resource('educators', EducatorController::class);
+//     Route::get('educators/all', [EducatorController::class, 'allEducators'])->name('educators.all');
+//     Route::resource('educators', EducatorController::class);
 
-    Route::resource('banners', BannerController::class);
+//     Route::resource('banners', BannerController::class);
 
-    Route::resource('roles', RolesPermissions::class);
+//     Route::resource('roles', RolesPermissions::class);
 
-    Route::controller(MessageController::class)->prefix('inbox')->name('inbox.')->group(function () {
-        Route::get('', 'index')->name('index');
-        Route::get('followers', 'getFollowers')->name('followers');
-        Route::get('messages', 'getMessages')->name('messages');
-        Route::post('message', 'sendMessage')->name('messages.send');
-    });
+//     Route::controller(MessageController::class)->prefix('inbox')->name('inbox.')->group(function () {
+//         Route::get('', 'index')->name('index');
+//         Route::get('followers', 'getFollowers')->name('followers');
+//         Route::get('messages', 'getMessages')->name('messages');
+//         Route::post('message', 'sendMessage')->name('messages.send');
+//     });
 
-    Route::controller(ProfileController::class)->prefix('profile')->name('profile.')->group(function () {
-        Route::get('profile', 'index')->name('index');
-        Route::post('update-password', 'updatePassword')->name('update.password');
-        Route::post('update-photo', 'updateProfileImage')->name('update.photo');
-        Route::post('upload-thumbnail', 'updateThumbnail')->name('update.thumbnail');
-        Route::post('update-profile', 'updateProfile')->name('update.profile');
-    });
+//     Route::controller(ProfileController::class)->prefix('profile')->name('profile.')->group(function () {
+//         Route::get('profile', 'index')->name('index');
+//         Route::post('update-password', 'updatePassword')->name('update.password');
+//         Route::post('update-photo', 'updateProfileImage')->name('update.photo');
+//         Route::post('upload-thumbnail', 'updateThumbnail')->name('update.thumbnail');
+//         Route::post('update-profile', 'updateProfile')->name('update.profile');
+//     });
 
-    Route::controller(CategoryController::class)->prefix('category')->name('category.')->group((function () {
-        Route::get('', 'index')->name('index');
-        Route::get('list', 'allCategories')->name('all');
-        Route::get('show/{id}', 'show')->name('show');
-        Route::post('store', 'store')->name('store');
-        Route::post('update/{id}', 'update')->name('update');
-        Route::post('/remove-image/{id}', 'removeImage')->name('remove.image');
-    }));
+//     Route::controller(CategoryController::class)->prefix('category')->name('category.')->group((function () {
+//         Route::get('', 'index')->name('index');
+//         Route::get('list', 'allCategories')->name('all');
+//         Route::get('show/{id}', 'show')->name('show');
+//         Route::post('store', 'store')->name('store');
+//         Route::post('update/{id}', 'update')->name('update');
+//         Route::post('/remove-image/{id}', 'removeImage')->name('remove.image');
+//     }));
 
-    Route::controller(AssetsController::class)->prefix('assets')->name('assets.')->group((function () {
-        Route::get('', 'index')->name('index');
-        Route::get('list', 'all')->name('all');
-        Route::get('show/{id}', 'show')->name('show');
-        Route::post('store', 'store')->name('store');
-        Route::post('update/{id}', 'update')->name('update');
-    }));
+//     Route::controller(AssetsController::class)->prefix('assets')->name('assets.')->group((function () {
+//         Route::get('', 'index')->name('index');
+//         Route::get('list', 'all')->name('all');
+//         Route::get('show/{id}', 'show')->name('show');
+//         Route::post('store', 'store')->name('store');
+//         Route::post('update/{id}', 'update')->name('update');
+//     }));
 
-    Route::controller(ScheduleController::class)->prefix('schedule')->name('schedule.')->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/all', 'schedules')->name('all');
-        Route::get('/videos/{schedule}', 'getVideos')->name('videos');
-        Route::post('/videos/favourite/{video}', 'makeFavourite')->name('videos.favourite');
-        Route::delete('/videos/delete/{video}', 'deleteVideo')->name('videos.delete');
-        Route::post('store', 'store')->name('store');
-        Route::get('show/{schedule}', 'show')->name('show');
-        Route::post('upload', 'uploadVideo')->name('upload');
-        Route::put('update/{schedule}', 'update')->name('update');
-        Route::post('live/start/{schedule}', 'startLive')->name('live.start');
-        Route::post('live/stop/{schedule}', 'stopLive')->name('live.stop');
-        Route::get('live/views/{schedule}', 'stopLive')->name('live.views');
-        Route::get('live/message', 'messages')->name('live.message');
-        Route::post('live/message', 'sendMessage')->name('live.message.send');
-        Route::get('live/count/{schedule}', 'getCounts')->name('live.count');
-        Route::delete('delete/{schedule}', 'destroy')->name('delete');
-    });
+//     Route::controller(ScheduleController::class)->prefix('schedule')->name('schedule.')->group(function () {
+//         Route::get('/', 'index')->name('index');
+//         Route::get('/all', 'schedules')->name('all');
+//         Route::get('/videos/{schedule}', 'getVideos')->name('videos');
+//         Route::post('/videos/favourite/{video}', 'makeFavourite')->name('videos.favourite');
+//         Route::delete('/videos/delete/{video}', 'deleteVideo')->name('videos.delete');
+//         Route::post('store', 'store')->name('store');
+//         Route::get('show/{schedule}', 'show')->name('show');
+//         Route::post('upload', 'uploadVideo')->name('upload');
+//         Route::put('update/{schedule}', 'update')->name('update');
+//         Route::post('live/start/{schedule}', 'startLive')->name('live.start');
+//         Route::post('live/stop/{schedule}', 'stopLive')->name('live.stop');
+//         Route::get('live/views/{schedule}', 'stopLive')->name('live.views');
+//         Route::get('live/message', 'messages')->name('live.message');
+//         Route::post('live/message', 'sendMessage')->name('live.message.send');
+//         Route::get('live/count/{schedule}', 'getCounts')->name('live.count');
+//         Route::delete('delete/{schedule}', 'destroy')->name('delete');
+//     });
 
-    Route::controller(AcademyController::class)->prefix('academy')->name('academy.')->group((function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/details/{academy}', 'details')->name('details');
-        Route::get('/all', 'all')->name('all');
-        Route::post('store', 'store')->name('store');
-        Route::get('show/{academy}', 'show')->name('show');
-        Route::put('update/{academy}', 'update')->name('update');
-        Route::delete('delete/{academy}', 'delete')->name('delete');
-    }));
+//     Route::controller(AcademyController::class)->prefix('academy')->name('academy.')->group((function () {
+//         Route::get('/', 'index')->name('index');
+//         Route::get('/details/{academy}', 'details')->name('details');
+//         Route::get('/all', 'all')->name('all');
+//         Route::post('store', 'store')->name('store');
+//         Route::get('show/{academy}', 'show')->name('show');
+//         Route::put('update/{academy}', 'update')->name('update');
+//         Route::delete('delete/{academy}', 'delete')->name('delete');
+//     }));
 
-    Route::controller(AcademyModuleController::class)->prefix('academy/modules')->name('academy.modules.')->group((function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/{category}', 'categoryModule')->name('categoryModule'); // This endpoint list all module under a category
-        Route::post('store', 'store')->name('store');
-        Route::get('details/{module}', 'details')->name('details');
-        Route::get('show/{module}', 'show')->name('show'); // This endpoint list all video under a module
-        Route::put('update/{module}', 'update')->name('update');
-        Route::put('make-video-favourite/{module}', 'makeFavourite')->name('makeFavourite');
-        Route::delete('delete/{module}', 'delete')->name('delete');
-    }));
+//     Route::controller(AcademyModuleController::class)->prefix('academy/modules')->name('academy.modules.')->group((function () {
+//         Route::get('/', 'index')->name('index');
+//         Route::get('/{category}', 'categoryModule')->name('categoryModule'); // This endpoint list all module under a category
+//         Route::post('store', 'store')->name('store');
+//         Route::get('details/{module}', 'details')->name('details');
+//         Route::get('show/{module}', 'show')->name('show'); // This endpoint list all video under a module
+//         Route::put('update/{module}', 'update')->name('update');
+//         Route::put('make-video-favourite/{module}', 'makeFavourite')->name('makeFavourite');
+//         Route::delete('delete/{module}', 'delete')->name('delete');
+//     }));
 
-    Route::controller(AcademyVideoController::class)->prefix('academy/videos')->name('academy.videos.')->group((function () {
-        Route::get('/{video}', 'index')->name('index');
-        Route::post('validate', 'validateVideoFile')->name('validate');
-        Route::post('store', 'store')->name('store');
-        Route::post('sort', 'updateOrder')->name('sort');
-        Route::put('update/{video}', 'update')->name('update');
-        Route::delete('delete/{video}', 'delete')->name('delete');
-    }));
-});
+//     Route::controller(AcademyVideoController::class)->prefix('academy/videos')->name('academy.videos.')->group((function () {
+//         Route::get('/{video}', 'index')->name('index');
+//         Route::post('validate', 'validateVideoFile')->name('validate');
+//         Route::post('store', 'store')->name('store');
+//         Route::post('sort', 'updateOrder')->name('sort');
+//         Route::put('update/{video}', 'update')->name('update');
+//         Route::delete('delete/{video}', 'delete')->name('delete');
+//     }));
+// });
 
-Route::get('test-notifications', function () {
-    event(new \App\Events\SignalNotification(1, "hello", "updated"));
+// Route::get('test-notifications', function () {
+//     event(new \App\Events\SignalNotification(1, "hello", "updated"));
 
-    return null;
-});
+//     return null;
+// });
 
-Route::get('push-notification', function () {
+// Route::get('push-notification', function () {
 
-    $firebaseToken = ["f-7-xyUjI05-g2xU13RXtr:APA91bHamFGUhQy476Forn8hKozkkGNqKOnPSTtA9xhjOpRCK1v-moHicASA-IEqXtyU_8wDx43US5apAeYq83iRjnImUPvfEDnunDvzWJR5vndvUQhMvZZMp4iEOWAUTUrOqPu2-sGd"];
+//     $firebaseToken = ["f-7-xyUjI05-g2xU13RXtr:APA91bHamFGUhQy476Forn8hKozkkGNqKOnPSTtA9xhjOpRCK1v-moHicASA-IEqXtyU_8wDx43US5apAeYq83iRjnImUPvfEDnunDvzWJR5vndvUQhMvZZMp4iEOWAUTUrOqPu2-sGd"];
 
-    $headers = [
-        'Authorization' => 'Bearer' . env('FIREBASE_SERVER_KEY'),
-        'Content-Type' => 'application/json',
-    ];
+//     $headers = [
+//         'Authorization' => 'Bearer' . env('FIREBASE_SERVER_KEY'),
+//         'Content-Type' => 'application/json',
+//     ];
 
-    $client = new \GuzzleHttp\Client([
-        'base_uri' => 'https://fcm.googleapis.com/v1/projects/delta-signal/messages:send',
-        // 'verify' => false,
-    ]);
+//     $client = new \GuzzleHttp\Client([
+//         'base_uri' => 'https://fcm.googleapis.com/v1/projects/delta-signal/messages:send',
+//         // 'verify' => false,
+//     ]);
 
-    $data = [
-        "message" => [
-            "token" => $firebaseToken,
-            "notification" => [
-                "title" => "Testing Notification",
-                "body" => "Testing Notification",
-            ],
-            "data" => [
-                "signals" => "hello"
-            ]
-        ]
-    ];
+//     $data = [
+//         "message" => [
+//             "token" => $firebaseToken,
+//             "notification" => [
+//                 "title" => "Testing Notification",
+//                 "body" => "Testing Notification",
+//             ],
+//             "data" => [
+//                 "signals" => "hello"
+//             ]
+//         ]
+//     ];
 
-    $data = json_encode($data);
+//     $data = json_encode($data);
 
-    $response = $client->post('send', [
-        'headers' => $headers,
-        'body' => $data,
-    ]);
+//     $response = $client->post('send', [
+//         'headers' => $headers,
+//         'body' => $data,
+//     ]);
 
-    // Process the response
-    $statusCode = $response->getStatusCode();
-    $responseData = $response->getBody()->getContents();
+//     // Process the response
+//     $statusCode = $response->getStatusCode();
+//     $responseData = $response->getBody()->getContents();
 
-    dd($responseData, true);
+//     dd($responseData, true);
 
 
-    // $data = [
-    //     'push_tokens' => $firebaseToken,
-    //     'title' => "Hello World",
-    //     'message' => "Trust your day is fine."
-    // ];
+//     // $data = [
+//     //     'push_tokens' => $firebaseToken,
+//     //     'title' => "Hello World",
+//     //     'message' => "Trust your day is fine."
+//     // ];
 
-    // $firebase = new \App\Services\PushNotification();
+//     // $firebase = new \App\Services\PushNotification();
 
-    // dd($firebase->sendNotification($data));
-});
+//     // dd($firebase->sendNotification($data));
+// });
