75b4a7b2125f64f136110acaec3a43e788f8cc9d
DEv Noel <ndamjoh@gmail.com>
2023-12-13 14:28:44 +0100

    throttle increased

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('throttle:60,1')->group(function () {
+Route::middleware('throttle:120,1')->group(function () {
 
     Route::post('/login', [AuthenticationController::class, 'login']);
     Route::post('/check-user', [AuthenticationController::class, 'getUserDetails']);
