6cdad0784a7166d6b5b0479cf5f09c9db0f700bb
DEv Noel <ndamjoh@gmail.com>
2024-04-18 12:09:58 +0100

    file upload updated

--- app/Http/Controllers/Admin/Academy/AcademyVideoController.php
+++ app/Http/Controllers/Admin/Academy/AcademyVideoController.php
@@ -62,23 +62,28 @@ class AcademyVideoController extends Controller
 
     public function validateVideoFile(Request $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:200', 'regex:/[^\s]+/'],
-            'module_uuid' => ['required', 'exists:academy_modules,uuid'],
-            'description' => ['nullable', 'max:10000', 'required_if:description,!=,null|regex:/[^\s]+/'],
-            'video_file' => ['required', 'mimes:mp4,avi,flv,mov,wmvp', 'max:512000'],
-            'length' => ['required', 'numeric', 'min:5'],
-        ]);
+        // $request->validate([
+        //     'name' => ['required', 'string', 'max:200', 'regex:/[^\s]+/'],
+        //     'module_uuid' => ['required', 'exists:academy_modules,uuid'],
+        //     'description' => ['nullable', 'max:10000', 'required_if:description,!=,null|regex:/[^\s]+/'],
+        //     'video_file' => ['required', 'mimes:mp4,avi,flv,mov,wmvp', 'max:512000'],
+        //     'length' => ['required', 'numeric', 'min:5'],
+        // ]);
 
-        return response()->json(['success' => true, 'message' => 'Validated successfully']);
+        // return response()->json(['success' => true, 'message' => 'Validated successfully']);
     }
 
     public function update(Request $request, AcademyVideo $video)
     {
-        $request->validate([
+
+        $validator = Validator::make([
             'name' => ['nullable', 'string', 'max:200'],
             'description' => ['nullable', 'max:10000'],
         ]);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
+        }
 
         $video->update([
             'name' => $request->name ?? $video->name,
@@ -90,9 +95,13 @@ class AcademyVideoController extends Controller
 
     public function updateOrder(Request $request)
     {
-        $this->validate($request, [
+        $validator = Validator::make([
             'videos.*.order' => 'required|numeric',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
+        }
 
         $videos = AcademyVideo::all();
 
