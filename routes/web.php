b6651e113cc811de3c4499e2ddb86789e3fef465
DEv Noel <ndamjoh@gmail.com>
2024-10-08 01:09:40 +0100

    tyuiop

--- routes/web.php
+++ routes/web.php
@@ -17,34 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('test', function () {
-    $data = [
-        'push_tokens' =>  ['f5uNZe_-tk03t6S-SUsdnw:APA91bFRU_20laXbsvFOkr2DMqKxUUmpmri6O-acLbjCvC7-VwGVloZYUBwjh_yB83MbCvD9Bs2nzZm0SKvDpRr0nwbOQsxN-FLnxXXXqvam3WC1-CoNygmVVlWNTy4GxAtedsjNlLbi'],
-        'title' => "Signal Created.",
-        'message' => "push notification test",
-    ];
-
-    dispatch(new PushNotificationJob($data));
-
-    dD("done");
-
-    // $firebase = new \App\Services\PushNotification();
-
-    // $data['push_token'] = "f5uNZe_-tk03t6S-SUsdnw:APA91bFRU_20laXbsvFOkr2DMqKxUUmpmri6O-acLbjCvC7-VwGVloZYUBwjh_yB83MbCvD9Bs2nzZm0SKvDpRr0nwbOQsxN-FLnxXXXqvam3WC1-CoNygmVVlWNTy4GxAtedsjNlLbi";
-    // $res = $firebase->sendNotification($data);
-
-    // dD($res);
-
-    // $res = [];
-
-    // foreach ($data['push_tokens'] as $token) {
-    //     $data['push_token'] = $token;
-    //     $firebase->sendNotification($data);
-    //     // dd($firebase);
-
-    //     // array_push($res, $firebase);
-    // }
-
-    // dD($res);
-});
