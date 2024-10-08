fbe02c9c4cff16d1aa03152bf3f5aa990ef397f1
DEv Noel <ndamjoh@gmail.com>
2024-10-08 13:03:04 +0100

    iopl

--- app/Http/Middleware/CustomSanctumAuth.php
+++ app/Http/Middleware/CustomSanctumAuth.php
@@ -22,81 +22,83 @@ class CustomSanctumAuth extends Middleware
     public function handle($request, Closure $next, ...$guards)
     {
         try {
-        // Retrieve the user ID from the custom header
-        $userId = $request->header('X-User-ID');
+            // Retrieve the user ID from the custom header
+            $userId = $request->header('X-User-ID');
 
-        if ($userId) {
-            // If X-User-ID header is present, use it for authentication
+            if ($userId) {
+                // If X-User-ID header is present, use it for authentication
 
-            $baseUrl = config('constants.auth.base_url');
+                $baseUrl = config('constants.auth.base_url');
 
-            // Make an API call to the user service to retrieve the user details
-            $response = Http::get("{$baseUrl}/user?user_id={$userId}");
+                // Make an API call to the user service to retrieve the user details
+                $response = Http::get("{$baseUrl}/user?user_id={$userId}");
 
-            // Check if the API call was successful
-            if ($response->failed()) {
-                return response()->json(['error' => 'Failed to retrieve user information'], 400);
+                // Check if the API call was successful
+                if ($response->failed()) {
+                    return response()->json(['error' => 'Failed to retrieve user information'], 400);
+                }
+
+                // Decode the response and check if the user exists
+                $user = (object) $response->json();
+
+
+                if (!$user || empty($user->data)) {
+                    return response()->json(['error' => 'Invalid user ID on headers'], 400);
+                }
+
+                $user = (object) $user->data;
+
+                // Update or create the user in the local database
+                $userp = User::where('email', $user->email)->orWhere('id', $user->user_id)->first();
+
+                if ($userp) {
+                    $userp->update([
+                        'uuid' => $user->id,
+                        'name' => $user->name,
+                        'username' => $user->username,
+                        'password' => "check", // You should set a proper password or handle it as per your requirement
+                        'role' => $user->role,
+                        'fcm_token' => $user->push_token,
+                        'profile_picture' => $user->profile_picture,
+                        'created_at' => $user->created_at,
+                    ]);
+                } else {
+                    $userp = User::create([
+                        'id' => $user->user_id,
+                        'uuid' => $user->id,
+                        'name' => $user->name,
+                        'email' =>  $user->email,
+                        'username' => $user->username,
+                        'password' => "check", // You should set a proper password or handle it as per your requirement
+                        'role' => $user->role,
+                        'fcm_token' => $user->push_token,
+                        'profile_picture' => $user->profile_picture,
+                        'created_at' => $user->created_at,
+                    ]);
+                }
+
+                $user = $userp;
+
+                // Attach the user to the request
+                $request->setUserResolver(function () use ($user) {
+                    return $user;
+                });
+
+                // Skip Sanctum authentication if X-User-ID is provided
+                return $next($request);
             }
 
-            // Decode the response and check if the user exists
-            $user = (object) $response->json();
+            // If X-User-ID is not provided, fall back to Sanctum authentication
 
-
-            if (!$user || empty($user->data)) {
-                return response()->json(['error' => 'Invalid user ID on headers'], 400);
+            if ($request->is('api/*')) {
+                $guards = ['sanctum'];
             }
 
-            $user = (object) $user->data;
+            // Call the parent handle method to use the Sanctum authentication logic
+            $this->authenticate($request, $guards);
 
-            // Update or create the user in the local database
-            $userp = User::where('email', $user->email)->orWhere('id', $user->user_id)->first();
-
-            if ($userp) {
-                $userp->update([
-                    'uuid' => $user->id,
-                    'name' => $user->name,
-                    'username' => $user->username,
-                    'password' => "check", // You should set a proper password or handle it as per your requirement
-                    'role' => $user->role,
-                    'profile_picture' => $user->profile_picture,
-                    'created_at' => $user->created_at,
-                ]);
-            } else {
-                $userp = User::create([
-                    'id' => $user->user_id,
-                    'uuid' => $user->id,
-                    'name' => $user->name,
-                    'email' =>  $user->email,
-                    'username' => $user->username,
-                    'password' => "check", // You should set a proper password or handle it as per your requirement
-                    'role' => $user->role,
-                    'profile_picture' => $user->profile_picture,
-                    'created_at' => $user->created_at,
-                ]);
-            }
-
-            $user = $userp;
-
-            // Attach the user to the request
-            $request->setUserResolver(function () use ($user) {
-                return $user;
-            });
-
-            // Skip Sanctum authentication if X-User-ID is provided
+            // Continue with the request
             return $next($request);
-        }
-
-        // If X-User-ID is not provided, fall back to Sanctum authentication
-
-        if ($request->is('api/*')) {
-            $guards = ['sanctum'];
-        }
-
-        // Call the parent handle method to use the Sanctum authentication logic
-        $this->authenticate($request, $guards);
-
-        // Continue with the request
-        return $next($request);
         } catch (\Exception $e) {
             sendToLog($e);
 
